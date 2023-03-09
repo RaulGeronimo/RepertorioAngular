6a28c804e96c1e86c11900022d77a8ef37a5765f
Raul Geronimo <raul090700@gmai.com>
2023-03-08 23:51:38 -0600

    Servidor

---
+++ BD/Datos.sql
@@ -0,0 +1,92 @@
+INSERT INTO Pais VALUES 
+(1,'Alemania','Aleman, Alemana','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/640px-Flag_of_Germany.svg.png'),
+(2,'Mexico','Mexicano, Mexicana','America','https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1280px-Flag_of_Mexico.svg.png'),
+(3,'Australia','Australiano, Australiana','Oceania','https://upload.wikimedia.org/wikipedia/commons/8/88/Flag_of_Australia_%28converted%29.svg'),
+(4,'Inglaterra','Ingles, Inglesa','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1920px-Flag_of_England.svg.png'),
+(5,'Estados Unidos','Americano, Americana','America','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png'),
+(6,'Francia','Frances, Francesa','Europa','https://m.media-amazon.com/images/I/41Dka5ibTAL.jpg'),
+(7,'China','Chino, China','Asia','https://genexus.blog/wp-content/uploads/2021/09/Bandera-China.jpeg'),
+(8,'Suecia','Sueco, Sueca','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Flag_of_Sweden.svg/1200px-Flag_of_Sweden.svg.png'),
+(9,'Rusia','Ruso, Rusa','Europa','https://mf.b37mrtl.ru/rbthmedia/images/2021.06/article/60c0e15285600a1fb24d33f7.jpg'),
+(10,'Canada','Canadiense','America','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/800px-Flag_of_Canada_%28Pantone%29.svg.png'),
+(11,'Argentina','Argentino','America','https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1200px-Flag_of_Argentina.svg.png'),
+(12,'Japon','Japones, Japonesa','Asia','https://setopen.sportdata.org/wkfranking/images/flags/JPN.gif'),
+(13,'Austria','Austriaco, Austriaca','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/640px-Flag_of_Austria.svg.png'),
+(14,'Belgica','Belga','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_Belgium_%28civil%29.svg/1200px-Flag_of_Belgium_%28civil%29.svg.png'),
+(15,'Irlanda del Norte','Norirlandes, Norirlandesa','Europa','https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Flag_of_Northern_Ireland_%281953%E2%80%931972%29.svg/1920px-Flag_of_Northern_Ireland_%281953%E2%80%931972%29.svg.png'),
+(16,'Nueva Zelanda','Neozelandes, Neozelandesa','Oceania','https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg');
+
+INSERT INTO Instrumento VALUES
+(NULL, 'Guitarra Acústica', 'La guitarra es un instrumento musical de la familia de los cordófonos, es decir los instrumentos que producen su sonido al hacer vibrar las cuerdas.', 'https://www.pngplay.com/wp-content/uploads/6/Real-Black-Acoustic-Guitar-PNG.png'),
+(NULL, 'Guitarra Eléctrica', 'La guitarra eléctrica es un tipo de guitarra en la que, a diferencia de la guitarra española y la guitarra sajona, su caja no hace resonancia, por lo que el sonido se produce indirectamente por inducción eléctrica y a través de un amplificador o también a través de un pedal de efectos.', 'https://www.pngall.com/wp-content/uploads/12/Electric-Guitar-Instrument-No-Background.png'),
+(NULL, 'Batería', 'La batería es un conjunto de instrumentos musicales de percusión usado por muchas agrupaciones musicales.', 'https://www.pngarts.com/files/3/Yamaha-Drum-Transparent-Images.png'),
+(NULL, 'Bajo Eléctrico', 'El bajo, bajo eléctrico o simplemente bajo, es el miembro más grave de la familia de las guitarras. Es un instrumento de cuerda pulsada similar en apariencia y construcción a una guitarra eléctrica o acústica, pero con un mástil y una escala más largos, y típicamente de cuatro a seis cuerdas o cursos.', 'https://i.pinimg.com/originals/96/d8/20/96d820eec771a2e48c3a55cf1f70e7e3.png'),
+(NULL, 'Ninguno / Vocalista', 'El micrófono es un transductor acústico eléctrico, es decir, transforma la señal acústica de nuestra voz en la señal eléctrica que reproduce el equipo al que se encuentra conectado. Los que utilizan el micrófono son los vocalistas de un grupo de Música, ya que no tocan algún instrumento musical.', 'https://www.pngplay.com/wp-content/uploads/9/Microphone-No-Background.png');
+
+INSERT INTO Artista VALUES
+(NULL, 'Kurt Donald Cobain', 'Kurt Cobain', 'H', '1967-02-20', '1994-04-05', 1.75, 5, 'Voz, Guitarra', 'Barítono ligero', 'https://pbs.twimg.com/media/Dz26hESUYAAtf9C?format=jpg&name=4096x4096'),
+(NULL, 'David Eric Grohl', 'Dave Grohl', 'H', '1969-01-14', NULL, 1.83, 5, 'Voz, Batería, Guitarra, Bajo eléctrico, Piano, Sintetizador', 'Tenor ligero', 'https://fondosmil.com/fondo/104285.jpg'),
+(NULL, 'Billie Joe Armstrong', 'Billie Joe Armstrong', 'H', '1972-02-17', NULL, 1.7, 5, 'Voz, guitarra, bajo, piano, mandolina, batería, armónica','Tenor', 'https://greendaylover.files.wordpress.com/2009/10/vma05_greendaybillyjoe.jpg'),
+(NULL, 'Chester Charles Bennington', 'Chester Bennington', 'H', '1973-03-20', '2017-07-20', 1.78, 5, 'Voz, guitarra, instrumento de percusión y piano', 'Tenor ligero, Gutural', 'https://www.teahub.io/photos/full/77-776215_chester-bennington.jpg');
+
+INSERT INTO Grupo VALUES
+(NULL, 'Nirvana', 'Aberdeen, Washington, Estados Unidos', 'Grunge, rock alternativo', '1987-10-03', '1994-04-05', 'Sub Pop, Tupelo Records, DGC Records, Geffen Records', 'Disuelto', 'http://nirvana.com/', 'Ingles', 'https://cdn.freebiesupply.com/logos/large/2x/nirvana-logo-png-transparent.png'),
+(NULL, 'Foo Fighters', 'Seattle, Washington, Estados Unidos', 'Hard rock, Post-grunge, Rock alternativo', '1994-07-16', NULL, 'RCA Records, Capitol Records, Roswell Records', 'Activo', 'http://foofighters.com/', 'Ingles', 'https://seeklogo.com/images/F/Foo_Fighters-logo-A7966CB57F-seeklogo.com.png'),
+(NULL, 'Green Day', 'Berkeley, California, Estados Unidos', 'Pop punk, Punk rock, Rock alternativo', '1986-10-12', NULL, 'Lookout Records, Reprise Records, Warner Bros Records', 'Activo', 'http://www.greenday.com/', 'Ingles', 'https://www.pngkey.com/png/full/230-2301807_green-day-png-vector-royalty-free-library-green.png'),
+(NULL, 'Linkin Park', 'Agoura Hills, California, Estados Unidos', '	Nu metal, rock alternativo, rap metal, metal alternativo, heavy metal, rock electrónico, pop', '1996-08-07', '2017-07-20', 'Warner Music Group, Warner Records, Machine Shop Recordings', 'En hiato', 'http://www.linkinpark.com/', 'Ingles', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/LPLogo-black.svg/1200px-LPLogo-black.svg.png');
+
+INSERT INTO Artista_Grupo VALUES
+(NULL, 1, 1, '1987-10-3', '1994-04-05', 2),
+(NULL, 2, 1, '1990-7-17', '1994-04-05', 3),
+(NULL, 2, 2, '1994-07-16', NULL, 2),
+(NULL, 3, 3, '1986-10-12', NULL, 2),
+(NULL, 4, 4, '1996-8-7', '2017-07-20', 5);
+
+INSERT INTO Disquera VALUES
+(NULL, 'DGC Records', '1990-03-20', 'David Geffen', 'Rock alternativo', 5, 'https://upload.wikimedia.org/wikipedia/en/f/fa/DGC_Records_logo%2C_1990.png'),
+(NULL, 'Sub Pop', '1986-7-16', 'Bruce Pavitt, Jonathan Poneman', 'Hip-hop, Indie pop, Punk rock, Heavy metal, Rock alternativo, Música de comedia', 5, 'https://cdn.freebiesupply.com/logos/large/2x/sub-pop-logo-png-transparent.png'),
+(NULL, 'RCA Records', '1990-01-09', 'Emile Berliner, Eldridge R. Johnson', 'Varios géneros', 5, 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/RCA_Records_%28logo%29.svg/1280px-RCA_Records_%28logo%29.svg.png'),
+(NULL, 'Warner Records', '1958-03-19', 'James Conkling', 'Varios', 5, 'https://variety.com/wp-content/uploads/2016/02/warner-records.jpg');
+
+INSERT INTO Album VALUES
+(NULL, 1, 2, 'Bleach', '00:42:14', '1989-06-15', '1988-1989', 'Grunge', 'https://m.media-amazon.com/images/I/71ThvtmT-FL._AC_SL1417_.jpg'),
+(NULL, 1, 1, 'Nevermind', '00:42:38', '1991-09-24', 'Mayo-junio de 1991', 'Grunge, rock alternativo', 'https://m.media-amazon.com/images/I/71DQrKpImPL._AC_SL1400_.jpg'),
+
+(NULL, 2, 3, 'Wasting Light', '00:47:56', '2011-04-12', '6 de septiembre de 2010 - 11 de diciembre de 2010', 'Post-grunge, hard rock y rock alternativo', 'https://m.media-amazon.com/images/I/81bcusKozsL._AC_SL1500_.jpg'),
+
+(NULL, 3, 4, 'American Idiot', '00:57:12', '2004-09-21', '18 de abril de 2003-26 de marzo de 2004 en Studio 880, Oakland, California; Ocean Way Recording, Hollywood', 'Pop punk, Punk rock, Rock alternativo, Ópera rock', 'https://m.media-amazon.com/images/I/71Z0rLIvpuL._AC_SL1448_.jpg'),
+(NULL, 3, 4, 'Revolution Radio', '00:44:29', '2016-11-11', 'noviembre de 2015 - junio de 2016 OTIS, Oakland', 'Pop punk, rock alternativo, punk rock', 'https://m.media-amazon.com/images/I/81+vo6G38sL._AC_SL1500_.jpg'),
+
+(NULL, 4, 4, 'Minutes to Midnight', '00:43:23', '2007-05-14', 'Enero de 2006 — marzo de 2007', 'Rock alternativo, metal alternativo', 'https://m.media-amazon.com/images/I/91esTaFkpPL._AC_SL1500_.jpg'),
+(NULL, 4, 4, 'Hybrid Theory', '00:37:48', '2000-10-24', 'Marzo-junio de 2000', 'Nu metal, rap metal, metal alternativo, rap rock', 'https://m.media-amazon.com/images/I/81iC+O0ec2L._AC_SL1448_.jpg'),
+(NULL, 4, 4, 'One More Light', '00:35:19', '2017-05-19', 'Noviembre de 2015-febrero de 2017', 'Pop, pop rock, electropop , Rock electrónico', 'https://ocdn.eu/images/pulscms/YzY7MDA_/85d9ab2d8707de5ea7705a70e4bb8727.jpeg');
+
+INSERT INTO Canciones VALUES
+(NULL, 'Come As You Are', '00:03:38', '1992-03-03', 'Grunge, rock alternativo', 'Ingles', 'Original', 1),
+(NULL, 'Smells Like Teen Spirit', '00:05:01', '1991-09-10', 'Grunge, rock alternativo, hard rock', 'Ingles', 'Original', 1),
+(NULL, 'About a Girl', '00:02:49', '1989-10-24', 'Rock alternativo', 'Ingles', 'Original', 1),
+
+(NULL, 'Walk ', '00:04:19', '2011-06-06', '	Rock alternativo, post-grunge y hard rock', 'Ingles', 'Original', 2),
+
+(NULL, 'American Idiot', '00:02:54', '2004-08-31', 'Pop punk', 'Ingles', 'Original', 3),
+(NULL, 'Whatsername', '00:04:12', '2004-09-20', 'Rock alternativo', 'Ingles', 'Original', 3),
+
+(NULL, 'Given up', '00:03:09', '2008-03-03', 'Metal alternativo, punk rock, hardcore punk, Post-Hardcore', 'Ingles', 'Original', 4),
+(NULL, 'What I''ve Done', '00:03:27', '2007-04-02', 'Rock alternativo, Hard rock', 'Ingles', 'Original', 4),
+(NULL, 'In the End', '00:03:37', '2001-10-09', 'Nu metal, rap rock, rock alternativo', 'Ingles', 'Original', 4),
+(NULL, 'One More Light', '00:04:15', '2017-10-03', 'Rock electrónico, balada', 'Ingles', 'Original', 4);
+
+INSERT INTO Canciones_Album VALUES
+(NULL, 2, 1,3),
+(NULL, 2, 2,1),
+(NULL, 1, 3,3),
+
+(NULL, 3, 4,11),
+
+(NULL, 4, 5,1),
+(NULL, 4, 6,9),
+
+(NULL, 6, 7,2),
+(NULL, 6, 8,6),
+(NULL, 7, 9,8),
+(NULL, 8, 10,9);
---
+++ BD/Repertorio.sql
@@ -0,0 +1,395 @@
+CREATE DATABASE Repertorio;
+
+USE Repertorio;
+
+CREATE TABLE Pais(
+    idPais INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Nacionalidad VARCHAR(70),
+    Continente VARCHAR(50),
+    Bandera TEXT
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Instrumento(
+    idInstrumento INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(70),
+    Descripcion TEXT,
+    Foto TEXT
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Artista(
+    idArtista INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(70),
+    NombreArtistico VARCHAR(50),
+    Genero CHAR,
+    FechaNacimiento DATE,
+    FechaFinado VARCHAR(50), /* DATE */
+    Estatura DOUBLE,
+    idNacionalidad INT,
+    Instrumentos VARCHAR(100),
+    TipoVoz VARCHAR(50),
+    Foto TEXT,
+    FOREIGN KEY (idNacionalidad) REFERENCES Pais(idPais) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Grupo(
+    idGrupo INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50),
+    Origen VARCHAR(200),
+    Genero VARCHAR(200),
+    Inicio DATE,
+    Fin VARCHAR(50), /* DATE */
+    Sellos VARCHAR(150),
+    Estado VARCHAR(50),
+    SitioWeb VARCHAR(50),
+    Idioma VARCHAR(50),
+    Logo TEXT
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Artista_Grupo(
+    Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    idArtista INT,
+    idGrupo INT,
+    FechaInicio DATE,
+    FechaFin VARCHAR(50), /* DATE */
+    idInstrumento INT,
+    FOREIGN KEY (idArtista) REFERENCES Artista(idArtista) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (idGrupo) REFERENCES Grupo(idGrupo) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (idInstrumento) REFERENCES Instrumento(idInstrumento) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Disquera(
+    idDisquera INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(60),
+    Fundacion DATE,
+    Fundador VARCHAR(100),
+    Generos VARCHAR(100),
+    idPais INT,
+    Logo TEXT,
+    FOREIGN KEY (idPais) REFERENCES Pais(idPais) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Album(
+    idAlbum INT PRIMARY KEY AUTO_INCREMENT,
+    idGrupo INT,
+    idDisquera INT,
+    Nombre VARCHAR(60),
+    Duracion TIME,
+    Lanzamiento DATE,
+    Grabacion VARCHAR(200),
+    Genero VARCHAR(100),
+    Portada TEXT,
+    FOREIGN KEY (idGrupo) REFERENCES Grupo(idGrupo) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (idDisquera) REFERENCES Disquera(idDisquera) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Canciones(
+    idCancion INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(70),
+    Duracion TIME,
+    Publicacion DATE,
+    Genero VARCHAR(100),
+    Idioma VARCHAR(50),
+    Interpretacion VARCHAR(50), /* cover, original, remake */
+    idGrupo INT,
+    FOREIGN KEY (idGrupo) REFERENCES Grupo(idGrupo) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE Canciones_Album(
+    Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    idAlbum INT,
+    idCancion INT,
+    Numero INT,
+    FOREIGN KEY (idAlbum) REFERENCES Album(idAlbum) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (idCancion) REFERENCES Canciones(idCancion) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+/* --------------------------------------------------------------------------------------- VISTAS --------------------------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- ARTISTA --------------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_Artista AS
+SELECT
+Artista.idArtista,
+Artista.Nombre,
+Artista.NombreArtistico,
+IF (Artista.Genero = 'H', 'Hombre', 'Mujer') AS Genero,
+DATE_FORMAT(Artista.FechaNacimiento, "%d / %M / %Y") AS FechaNacimiento,
+DATE_FORMAT(Artista.FechaFinado, "%d / %M / %Y") AS FechaFinado,
+CASE
+WHEN Artista.FechaFinado IS NULL OR Artista.FechaFinado <= 0 THEN CONCAT_WS(' ', TIMESTAMPDIFF(YEAR, Artista.FechaNacimiento, NOW()), 'años')
+WHEN Artista.FechaFinado <= 0 THEN 'Fecha Invalida'
+WHEN Artista.FechaNacimiento <= Artista.FechaFinado THEN CONCAT_WS(' ', TIMESTAMPDIFF(YEAR, Artista.FechaNacimiento, Artista.FechaFinado), 'años')
+ELSE 'Fecha Invalida'
+END AS Edad,
+FORMAT(Artista.Estatura, 2) AS Estatura,
+CONCAT_WS(' - ', Pais.Nombre, Pais.Nacionalidad) AS Pais,
+Artista.Instrumentos,
+Artista.TipoVoz,
+Artista.Foto
+FROM Artista
+INNER JOIN Pais
+ON Artista.idNacionalidad = Pais.idPais
+ORDER BY Nombre;
+
+/* ---------------------------------------------------------------------- GRUPO ---------------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_Grupo AS
+SELECT 
+G.idGrupo,
+G.Nombre,
+SUM(Albumes) AS Albumes,
+SUM(Cancion) AS Cancion,
+G.Origen,
+G.Genero,
+DATE_FORMAT(G.Inicio, "%d / %M / %Y") AS FechaInicio,
+DATE_FORMAT(G.Fin, "%d / %M / %Y") AS FechaFin,
+DATE_FORMAT(G.Inicio, '%Y-%m-%d') AS Inicio,
+G.Fin AS Fin,
+G.Sellos,
+G.Estado,
+G.SitioWeb,
+G.Idioma,
+G.Logo
+FROM(
+SELECT 
+a.idGrupo,
+a.Nombre,
+COUNT(idAlbum) AS Albumes,
+0 AS Cancion,
+a.Origen,
+a.Genero,
+DATE_FORMAT(a.Inicio, "%d / %M / %Y") AS FechaInicio,
+DATE_FORMAT(a.Fin, "%d / %M / %Y") AS FechaFin,
+DATE_FORMAT(a.Inicio, '%Y-%m-%d') AS Inicio,
+a.Fin AS Fin,
+a.Sellos,
+a.Estado,
+a.SitioWeb,
+a.Idioma,
+a.Logo
+FROM Grupo AS a
+LEFT JOIN Album as c2
+ON a.idGrupo = c2.idGrupo
+GROUP BY (a.idGrupo)
+
+UNION ALL
+
+SELECT
+b.idGrupo,
+b.Nombre,
+0 AS Albumes,
+COUNT(idCancion) AS Cancion,
+b.Origen,
+b.Genero,
+DATE_FORMAT(b.Inicio, "%d / %M / %Y") AS FechaInicio,
+DATE_FORMAT(b.Fin, "%d / %M / %Y") AS FechaFin,
+DATE_FORMAT(b.Inicio, '%Y-%m-%d') AS Inicio,
+b.Fin AS Fin,
+b.Sellos,
+b.Estado,
+b.SitioWeb,
+b.Idioma,
+b.Logo
+FROM Grupo AS b
+LEFT JOIN Canciones AS c1
+ON b.idGrupo = c1.idGrupo
+GROUP BY (b.idGrupo)
+) AS G 
+GROUP BY G.idGrupo;
+
+/* ------------------------------------------------------------------ ARTISTA GRUPO ------------------------------------------------------------------ */
+CREATE OR REPLACE VIEW
+Vista_GrupoIntegrantes AS
+SELECT
+Artista_Grupo.Codigo,
+Artista.idArtista,
+Artista.Nombre,
+Artista.NombreArtistico,
+IF (Artista.Genero = 'H', 'Hombre', 'Mujer') AS Genero,
+DATE_FORMAT(Artista.FechaNacimiento, "%d / %M / %Y") AS FechaNacimiento,
+DATE_FORMAT(Artista.FechaFinado, "%d / %M / %Y") AS FechaFinado,
+
+CASE
+WHEN Artista.FechaFinado IS NULL OR Artista.FechaFinado <= 0 THEN CONCAT_WS(' ', TIMESTAMPDIFF(YEAR, Artista.FechaNacimiento, NOW()), 'años')
+WHEN Artista.FechaFinado <= 0 THEN 'Fecha Invalida'
+WHEN Artista.FechaNacimiento <= Artista.FechaFinado THEN CONCAT_WS(' ', TIMESTAMPDIFF(YEAR, Artista.FechaNacimiento, Artista.FechaFinado), 'años')
+ELSE 'Fecha Invalida'
+END AS Edad,
+
+FORMAT(Artista.Estatura, 2) AS Estatura,
+CONCAT_WS(' - ', Pais.Nombre, Pais.Nacionalidad) AS Pais,
+Instrumento.Nombre AS Instrumento,
+Artista.TipoVoz,
+Artista.Foto,
+DATE_FORMAT(Artista_Grupo.FechaInicio, "%d / %M / %Y") AS FechaInicio,
+DATE_FORMAT(Artista_Grupo.FechaFin, "%d / %M / %Y") AS FechaFin,
+
+CASE
+WHEN Artista_Grupo.FechaFin IS NULL OR Artista_Grupo.FechaFin <= 0 THEN CONCAT_WS(' - ', YEAR(Artista_Grupo.FechaInicio), 'Actualidad')
+ELSE CONCAT_WS(' - ', YEAR(Artista_Grupo.FechaInicio), YEAR(Artista_Grupo.FechaFin))
+END AS Periodo,
+
+Grupo.idGrupo,
+Grupo.Nombre AS Grupo
+FROM Artista
+INNER JOIN Pais
+ON Artista.idNacionalidad = Pais.idPais
+
+LEFT JOIN Artista_Grupo
+ON Artista.idArtista = Artista_Grupo.idArtista
+
+LEFT JOIN Grupo
+ON Grupo.idGrupo = Artista_Grupo.idGrupo
+
+INNER JOIN Instrumento
+ON Artista_Grupo.idInstrumento = Instrumento.idInstrumento
+
+ORDER BY Nombre, FechaInicio DESC;
+
+/* --------------------------------------------------------------------- DISQUERA --------------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_Disquera AS
+SELECT
+Disquera.idDisquera,
+Disquera.Nombre,
+DATE_FORMAT(Disquera.Fundacion, "%M / %Y") AS Fundacion,
+Disquera.Fundador,
+Disquera.Generos,
+Pais.Nombre AS Pais,
+Disquera.Logo
+FROM Disquera
+INNER JOIN Pais
+ON Disquera.idPais = Pais.idPais
+ORDER BY Nombre;
+
+/* ---------------------------------------------------------------------- ALBUM ---------------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_Album AS
+SELECT
+Album.idAlbum,
+Album.idGrupo,
+Album.idDisquera,
+Grupo.Nombre AS Grupo,
+Disquera.Nombre AS Disquera,
+Album.Nombre AS Nombre,
+CONCAT_WS(' - ', Album.Nombre, Grupo.Nombre) AS Album,
+COUNT(Canciones_Album.idCancion) AS Canciones,
+IF(DATE_FORMAT(Album.Duracion, "%H") = '00', DATE_FORMAT(Album.Duracion, "%i:%s"), DATE_FORMAT(Album.Duracion, "%H:%i:%s")) AS DuracionF,
+Album.Duracion AS Duracion,
+DATE_FORMAT(Album.Lanzamiento, "%Y-%m-%d") AS Lanzamiento,
+DATE_FORMAT(Album.Lanzamiento, "%d / %M / %Y") AS FechaLanzamiento,
+Album.Grabacion,
+Album.Genero,
+Album.Portada
+FROM Album
+INNER JOIN Grupo
+ON Album.idGrupo = Grupo.idGrupo
+
+INNER JOIN Disquera
+ON Album.idDisquera = Disquera.idDisquera
+
+LEFT JOIN Canciones_Album
+ON Album.idAlbum = Canciones_Album.idAlbum
+
+GROUP BY(Album.idAlbum)
+ORDER BY (Album.Nombre);
+
+/* -------------------------------------------------------------------- CANCIONES -------------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_Canciones AS
+SELECT
+Canciones.idCancion,
+Canciones.Nombre,
+DATE_FORMAT(Canciones.Duracion, "%i:%s") AS Duracion,
+DATE_FORMAT(Canciones.Publicacion, "%d / %M / %Y") AS Publicacion,
+CONCAT_WS(' - ', Canciones.Nombre, Grupo.Nombre) AS Cancion,
+Canciones.Genero,
+Canciones.Idioma
+FROM Canciones
+INNER JOIN Grupo
+ON Grupo.idGrupo = Canciones.idGrupo
+ORDER BY Nombre;
+
+/* ----------------------------------------------------------------- CANCIONES ALBUM ----------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_CancionesAlbum AS
+SELECT
+Canciones_Album.Codigo,
+Canciones.idCancion,
+Album.idAlbum,
+Album.Nombre AS Album,
+Canciones_Album.Numero,
+Canciones.Nombre,
+DATE_FORMAT(Canciones.Duracion, "%i:%s") AS Duracion,
+DATE_FORMAT(Publicacion, "%d / %M / %Y") AS Publicacion,
+Canciones.Genero,
+Idioma
+FROM Canciones
+INNER JOIN Canciones_Album
+ON Canciones.idCancion = Canciones_Album.idCancion
+INNER JOIN Album
+ON Album.idAlbum = Canciones_Album.idAlbum
+ORDER BY Nombre;
+
+/* ----------------------------------------------------------------- CANCIONES GRUPO ----------------------------------------------------------------- */
+CREATE OR REPLACE VIEW
+Vista_CancionesGrupo AS
+SELECT
+Canciones.idCancion,
+Grupo.idGrupo,
+Grupo.Nombre AS Grupo,
+Canciones.Nombre,
+DATE_FORMAT(Canciones.Duracion, "%i:%s") AS Duracion,
+DATE_FORMAT(Canciones.Publicacion, "%d / %M / %Y") AS Publicacion,
+Canciones.Genero,
+Canciones.Idioma,
+Canciones.Interpretacion
+FROM Canciones
+INNER JOIN Grupo
+ON Grupo.idGrupo = Canciones.idGrupo;
+
+/* ----------------------------------------------------------------------------- PROCEDIMIENTOS ALMACENADOS ----------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------ ARTISTA GRUPO ------------------------------------------------------------------ */
+DELIMITER $$
+CREATE PROCEDURE `obtener_integrantes`(IN idGrupoB INT)
+BEGIN
+SELECT * FROM Vista_GrupoIntegrantes WHERE idGrupo = idGrupoB;
+END$$
+
+DELIMITER ;
+
+/* ------------------------------------------------------------------- ALBUM GRUPO ------------------------------------------------------------------- */
+DELIMITER $$
+CREATE PROCEDURE `obtener_album`(IN idGrupoB INT)
+BEGIN
+SELECT * FROM Vista_Album WHERE idGrupo = idGrupoB ORDER BY Lanzamiento;
+END$$
+
+DELIMITER ;
+
+/* ----------------------------------------------------------------- CANCIONES ALBUM ----------------------------------------------------------------- */
+DELIMITER $$
+CREATE PROCEDURE `obtener_cancionesAlbum`(IN idAlbumA INT)
+BEGIN
+SELECT * FROM Vista_CancionesAlbum WHERE idAlbum = idAlbumA ORDER BY Numero;
+END$$
+
+DELIMITER ;
+
+/* ----------------------------------------------------------------- CANCIONES GRUPO ----------------------------------------------------------------- */
+DELIMITER $$
+CREATE PROCEDURE `obtener_canciones`(IN idGrupoB INT)
+BEGIN
+SELECT * FROM Vista_CancionesGrupo WHERE idGrupo = idGrupoB;
+END$$
+
+DELIMITER ;
